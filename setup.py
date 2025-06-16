ddb0ef2a8eef71790366c1bb572e7a69a8ccfb6f
Harsh Chaudhary <hchaudhary1901.com>
2025-06-16 11:25:35 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup 
+
+setup(
+    name="Generative Ai Projects",
+    version='0.0.0',
+    author='Harsh',
+    author_email='hchaudhary1901@gmail.com',
+    packages=find_packages(),
+    install_requires= []
+)
